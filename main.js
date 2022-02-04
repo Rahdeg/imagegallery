9b28e802aa40c7485e6b46c6cb06b5d07d304a1c
Rahdeg <61963842+Rahdeg@users.noreply.github.com>
2022-02-04 01:36:10 +0100

    revision

--- main.js
+++ main.js
@@ -1,31 +1,29 @@
-const displayedImage = document.querySelector('.displayed-img');
-const thumbBar = document.querySelector('.thumb-bar');
+let overlay= document.querySelector(".overlay");
+let midpictures= document.querySelector(".midpix");
+let displayedImage= document.querySelector(".displayimages");
+let btn= document.querySelector("button");
 
-const btn = document.querySelector('button');
-const overlay = document.querySelector('.overlay');
-
-/* Looping through images */
-
-for (var i = 1; i <= 5; i++) {
-const newImage = document.createElement('img');
-newImage.setAttribute('src', "images/pic"+ i +".jpg");
-thumbBar.appendChild(newImage);
-newImage.onclick=function (e) {
-	displayedImage.src=e.target.src;
-}
+// loping through image
+for (let i = 1; i <= 5; i++) {
+	let imgs= document.createElement("img");
+	imgs.setAttribute("src", "images/pic" + i + ".jpg");
+	midpictures.appendChild(imgs);
+	imgs.onclick=(e)=>{
+		displayedImage.src=e.target.src;	
+	}
 }
 
+// lighten and darken
 
-/* Wiring up the Darken/Lighten button */
-btn.onclick=function () {
+btn.onclick=()=>{
 	let btnClass=btn.getAttribute("class");
-if (btnClass==="dark") {
-	btn.setAttribute("class","light");
-	btn.textContent="lighten";
-	overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
-}else{
-	btn.setAttribute("class","dark");
-	btn.textContent="Darken";
-	overlay.style.backgroundColor = 'rgba(0,0,0,0)';
-}
-}
+	if (btnClass==="dark") {
+		btn.setAttribute("class","light");
+		btn.textContent="lighten";
+		overlay.style.backgroundColor = 'rgba(0,0,0,0.5)';
+	}else{
+		btn.setAttribute("class","dark");
+		btn.textContent="Darken";
+		overlay.style.backgroundColor = 'rgba(0,0,0,0)';
+	}
+	}
